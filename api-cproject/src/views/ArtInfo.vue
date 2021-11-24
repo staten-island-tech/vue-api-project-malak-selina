cb82c9a16bbd8ec47cd211109104290ece9e1191
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2021-11-24 11:36:08 -0500

    YAY

--- api-cproject/src/views/ArtInfo.vue
+++ api-cproject/src/views/ArtInfo.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="info">
-      
-    <h2>{{ art.data.artist_title }}</h2>
-  <ul><li v-for="cat in art.data.category_ids" :key="cat">{{cat}}</li></ul>
+    <h2>{{ art.data.title }}</h2>
+     <h2>{{ art.data.department_title }}</h2>
+    <div class="chart">
+        <div class="leftchart">
+            <ul>
+                <li class="left">Artist</li>
+                <li class="left">Title</li>
+                <li class="left">Origin</li>
+                <li class="left">Date</li>
+                <li class="left">Medium</li>
+            </ul>
+        </div>
+        <div class="rightchart">
+            <ul>
+                <li class="right">{{ art.data.artist_title }}</li>
+                <li class="right">{{ art.data.title }}</li>
+                <li class="right">{{ art.data.place_of_origin }}</li>
+                <li class="right">{{ art.data.date_end }}</li>
+                <li class="right">{{ art.data.medium_display }}</li>
+            </ul>
+        </div>
+    </div>
+
   </div>
 </template>
 
@@ -27,6 +47,7 @@ export default {
     created(){
         this.getData()
     },  
+
     // getArts() {
     //     return this.arts.get('/information')
     //  },
@@ -40,5 +61,18 @@ export default {
 <style scoped>
     .info{
         color:black;
+    }
+    .chart{
+        display: flex;
+        
+    }
+    ul{
+        list-style-type: none;
+    }
+    .leftchart,
+    .rightchart{
+        display:flex;
+        flex-direction: column;
+
     }
 </style>
